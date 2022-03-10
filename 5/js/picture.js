12d512cb53ce66e53fe2ceff43a933ebbeecd0fc
Keks <keks@htmlacademy.ru>
2022-03-10 18:26:57 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/picture.js
@@ -0,0 +1,23 @@
+import {createRandomDescription} from './data.js';
+
+// Находим фрагмент шаблона фотографии
+const templateFragment = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+//Функция по загрузке данных в клон шаблона #picture
+const getPhotoObject = () => {
+  const photo = templateFragment.cloneNode(true);
+  const photoSrc = photo.querySelector('picture-img');
+  const photoComments = photo.querySelector('picture__comments');
+  const photoLikes = photo.querySelector('picture__likes');
+
+  photoSrc.src = createRandomDescription.url;
+  photoComments.textContent = createRandomDescription.comments;
+  photoLikes.textContent = createRandomDescription.likes;
+
+  return photo;
+};
+getPhotoObject();
+
+export {getPhotoObject};
