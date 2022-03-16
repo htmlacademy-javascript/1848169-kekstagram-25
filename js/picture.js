ae582fcb2133ce5ce7d64312fa1cacd9b6781a29
Rustem <arr747@mail.ru>
2022-03-16 22:16:02 +0500

    redact class

--- js/picture.js
+++ js/picture.js
@@ -9,9 +9,9 @@ const photosBlock = document.querySelector('.pictures');
 // Заполнение шаблона документа
 const getPhotoItem = (photoObject) => {
   const photoItem = templateFragment.cloneNode(true);
-  photoItem.querySelector('picture__img').src = photoObject.url;
-  photoItem.querySelector('picture__likes').textContent = photoObject.likes;
-  photoItem.querySelector('picture__comments').textContent = photoObject.comments.length;
+  photoItem.querySelector('.picture__img').src = photoObject.url;
+  photoItem.querySelector('.picture__likes').textContent = photoObject.likes;
+  photoItem.querySelector('.picture__comments').textContent = photoObject.comments.length;
   return photoItem;
 };
 
