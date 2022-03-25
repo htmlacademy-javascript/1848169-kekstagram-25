83e4d9eeddad21b57110847b7ed63f29de5edd39
Rustem <arr747@mail.ru>
2022-03-25 11:39:37 +0500

    redact validation, form

--- js/validation.js
+++ js/validation.js
@@ -35,23 +35,12 @@ const onCommentTextareaInput = () => {
     commentTextarea.setCustomValidity(errorMessages.COMMENT_LONG);
   }
 };
-// Выделение поля ввода
-const lightInput = (input) => {
-  input.style.border = '3px solid green';
-  input.style.boxShadow = 'none';
-};
-
-// Убираем подсветку инпута
-const resetlightInput = (input) => {
-  input.style.border = '';
-};
 
 //Проверка повторного использования хэштэга
 const isHashtagAgain = (item, index, items) => items.indexOf(item) === index;
 
 // Обработчик ввода хэштегов
 const onInputHashtags = () => {
-  resetlightInput(inputHashtags);
   const hashtags = inputHashtags.value.trim().toLowerCase().split(' ');
 
   if (!hashtags.length) {
@@ -59,34 +48,27 @@ const onInputHashtags = () => {
   }
 
   if (hashtags.length > hashtagsFeatures.MAX_NUMBER) {
-    lightInput(inputHashtags);
     inputHashtags.setCustomValidity(errorMessages.OVER_MAX);
     return;
   }
 
   if (!hashtags.every(isHashtagAgain)) {
-    lightInput(inputHashtags);
     inputHashtags.setCustomValidity(errorMessages.UNIQUE);
     return;
   }
 
   hashtags.forEach((hashtag) => {
     if (hashtag.length > hashtagsFeatures.MAX) {
-      lightInput(inputHashtags);
       inputHashtags.setCustomValidity(errorMessages.HASHTAGS_LONG);
     } else if (/^#?а-яёа-zd]+$/.test(hashtag)) {
       inputHashtags.setCustomValidity(errorMessages.BAG_SYMBOL_MESSAGE);
     } else if (hashtag === '#') {
-      lightInput(inputHashtags);
       inputHashtags.setCustomValidity(errorMessages.TOO_SHORT);
     } else if (hashtag.charAt(0) !== '#') {
-      lightInput(inputHashtags);
       inputHashtags.setCustomValidity(errorMessages.HASH_SYMBOL);
     } else if (hashtag.includes('#', 1)) {
-      lightInput(inputHashtags);
       inputHashtags.setCustomValidity(errorMessages.SPACE_DELIMITER);
     } else {
-      resetlightInput(inputHashtags);
       inputHashtags.setCustomValidity('');
     }
   });
