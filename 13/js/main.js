9bd872394e827c840023a0f9ce5d0d1975a2bc96
Keks <keks@htmlacademy.ru>
2022-04-04 18:22:37 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,8 @@
+import {getObjectOutput} from './data.js';
+import {createPhotosFragment} from './picture.js';
+import './form.js';
+
+const photosData = getObjectOutput();
+createPhotosFragment(photosData);
+
+
