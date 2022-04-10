b36835dd24e2e73da0392abf6f9ffeac1bda7439
Rustem <arr747@mail.ru>
2022-04-10 16:53:01 +0500

    create network

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {getObjectOutput} from './data.js';
 import {createPhotosFragment} from './picture.js';
 import './form.js';
+import './scale.js';
+import './slider.js';
 
 const photosData = getObjectOutput();
 createPhotosFragment(photosData);
