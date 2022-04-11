78f52fee2d513b9be8807e19971b6912f8df3f07
Keks <keks@htmlacademy.ru>
2022-04-11 07:46:29 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/form.js
@@ -0,0 +1,49 @@
+import {getEscapeEvent} from './util.js';
+import './validation.js';
+
+//Описание переменных
+const body = document.body;
+const uploadFileInput = document.querySelector('#upload-file');
+const uploadForm = document.querySelector('.img-upload__form');
+const editImageOverlay = document.querySelector('.img-upload__overlay');
+const editImageCloseButton = editImageOverlay.querySelector('.img-upload__cancel');
+const inputHashtags = document.querySelector('.text__hashtags');
+const commentTextarea = document.querySelector('.text__description');
+
+//Функция открытия окна редактирования
+const openImageEditOverlay = () => {
+  body.classList.add('modal-open');
+  editImageOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onImageOverlayEscPress);
+  editImageCloseButton.addEventListener('click', onImageOverlayClose);
+};
+
+//Обработчик открытия окна редактирования
+function onUploadChange () {
+  openImageEditOverlay();
+}
+uploadFileInput.addEventListener('change', onUploadChange);
+
+//Функция закрытия окна редактора
+const editImageOverlayClose = () => {
+  editImageOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uploadForm.reset();
+  document.removeEventListener('keydown', onImageOverlayEscPress);
+  editImageCloseButton.removeEventListener('click', onImageOverlayClose);
+};
+
+//Обработчик закрытия окна редактирования
+function onImageOverlayClose () {
+  editImageOverlayClose();
+}
+
+//Функция закрытия окна редактора по ESC
+function onImageOverlayEscPress (evt) {
+  const active = document.activeElement;
+  if (inputHashtags !== active && commentTextarea !== active) {
+    getEscapeEvent(evt, editImageOverlayClose);
+  }
+}
+
+export {onImageOverlayClose};
---
+++ 15/js/util.js
@@ -0,0 +1,68 @@
+const ALERT_SHOW_TIME = 5000;
+
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandom = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if(min < 0) {
+    min = Math.abs(min);
+  }
+  if(max < 0) {
+    max = Math.abs(max);
+  }
+  if (max <= min ) {
+    max = min;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна
+const getLength = (stringChecked, maxLength) => stringChecked.length <= maxLength;
+
+//Генерация числа из массива
+let RandomNumber;
+const getRandomNumber = (min, max) => {
+  const arr = [];
+  for (let i = min; i < max; i++) {
+    RandomNumber = getRandom(min, max);
+    if (arr.includes(RandomNumber) === true) {
+      i = i - 1;
+    } else {
+      if (RandomNumber > max === false) {
+        arr.push(RandomNumber);
+      }
+    }
+  }
+  return RandomNumber;
+};
+
+// Функция закрытия окна по Escape
+const getEscapeEvent = (evt, action) => {
+  if (evt.key === 'Escape') {
+    action();
+  }
+};
+
+//Показ сообщения об отправке с ошибкой на 5 секунд
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandom, getRandomNumber, getLength, getEscapeEvent, showAlert};
